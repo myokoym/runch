7a4514f93f33d8c924c77b182210879d9ac953da
Masafumi Yokoyama <myokoym@gmail.com>
2013-07-23 00:41:05 +0900

    Indent

--- lib/runch/language/base.rb
+++ lib/runch/language/base.rb
@@ -3,34 +3,34 @@ require "tmpdir"
 module Runch
   module Language
     module Base
-    def initialize(main_source, files)
-      @main_source = main_source
-      @files = files
-    end
-
-    def run
-      Dir.mktmpdir("runch") do |tmpdir|
-        copy(tmpdir)
-        executable = compile(tmpdir)
-        execute(executable)
+      def initialize(main_source, files)
+        @main_source = main_source
+        @files = files
       end
-    end
 
-    private
-    def copy(tmpdir)
-      FileUtils.cp(@main_source, tmpdir)
-      @files.each do |file|
-        FileUtils.cp_r(file, tmpdir)
+      def run
+        Dir.mktmpdir("runch") do |tmpdir|
+          copy(tmpdir)
+          executable = compile(tmpdir)
+          execute(executable)
+        end
       end
-    end
 
-    def compile(tmpdir)
-      @main_source
-    end
+      private
+      def copy(tmpdir)
+        FileUtils.cp(@main_source, tmpdir)
+        @files.each do |file|
+          FileUtils.cp_r(file, tmpdir)
+        end
+      end
 
-    def execute(executable)
-      system(executable)
-    end
+      def compile(tmpdir)
+        @main_source
+      end
+
+      def execute(executable)
+        system(executable)
+      end
     end
   end
 end
