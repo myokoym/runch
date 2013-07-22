3566b58649c190beb83d394c0e5bd7109128311d
Masafumi Yokoyama <myokoym@gmail.com>
2013-07-22 22:53:29 +0900

    Extract methods from a run method

--- lib/runch/language.rb
+++ lib/runch/language.rb
@@ -9,18 +9,31 @@ module Runch
 
     def run
       Dir.mktmpdir("runch") do |dir|
+        copy(dir)
+        executable = compile(dir)
+        execute(executable)
+      end
+    end
+
+    private
+    def copy(dir)
         FileUtils.cp(@main_source, dir)
         @files.each do |file|
           FileUtils.cp_r(file, dir)
         end
+    end
 
+    def compile(dir)
         basename = File.basename(@main_source, ".*")
         filename = File.basename(@main_source)
         main_source = File.join(dir, filename)
         executable = File.join(dir, basename)
         system("gcc", "-o", executable, main_source)
+        executable
+    end
+
+    def execute(executable)
         system(executable)
-      end
     end
   end
 end
