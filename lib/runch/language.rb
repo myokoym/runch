15a206bb541d8c8a3219cc7e90ba0a695a14f9cd
Masafumi Yokoyama <myokoym@gmail.com>
2013-07-22 22:00:37 +0900

    Extract code from command class

---
+++ lib/runch/language.rb
@@ -0,0 +1,19 @@
+require "tmpdir"
+
+module Runch
+  class Language
+    def self.run(arguments)
+      Dir.mktmpdir("runch") do |dir|
+        arguments.each do |file|
+          FileUtils.cp(file, dir)
+          basename = File.basename(file, ".*")
+          filename = File.basename(file)
+          main_source = File.join(dir, filename)
+          executable = File.join(dir, basename)
+          system("gcc", "-o", executable, main_source)
+          system(executable)
+        end
+      end
+    end
+  end
+end
